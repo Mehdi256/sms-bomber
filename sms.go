410007d2916bb2ae517924a0a26fa1ede67fb4b3
Mehdi <79555777+Mehdi256@users.noreply.github.com>
2025-04-09 15:43:07 +0330

    Update sms.go

--- sms.go
+++ sms.go
@@ -53,12 +53,12 @@ func main() {
 	fmt.Println("\033[01;33m")
 	fmt.Println(` 
                                                                    
-            Mehdi 256 Mehdi 256                                                                                                                                         												 
+             Mehdi 256 Mehdi 256                                                                                                                                         												 
 
 	`)
 	var phone string
 	fmt.Println("\033[01;31m[\033[01;32m+\033[01;31m] \033[01;33mSms bomber ! number web service : \033[01;31m177 \n\033[01;31m[\033[01;32m+\033[01;31m] \033[01;33mCall bomber ! number web service : \033[01;31m6\n\n")
-	fmt.Print("\033[01;31m[\033[01;32m+\033[01;31m] \033[01;32mEnter phone [Ex : 09xxxxxxxxxx]: \033[00;36m")
+	fmt.Print("\033[01;31m[\033[01;32m+\033[01;31m] \033[01;32mEnter phone [Ex:09xxxx]: \033[00;36m")
 	fmt.Scan(&phone)
 
 	var repeatCount int
